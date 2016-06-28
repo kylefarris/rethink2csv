b57dc800e5253131247b4e6e496054b5d247821b
Kyle Farris <kyle.farris@infotechfl.com>
2016-06-27 22:05:36 -0400

    Bugs squashed. I think we now have an acceptably working first version.

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ var split = function() {
     return through.obj(function (array, enc, cb) {
         array.forEach(this.push.bind(this));
         cb(null);
-    })
+    });
 };
 
+// ****************************************************************************
+// Class definition and contructor
+// ****************************************************************************
 function Rethink2Csv(opts) {
     var defaults = {
         csv: {
@@ -23,9 +26,17 @@ function Rethink2Csv(opts) {
     this.csv_opts = Object.assign({}, defaults, opts);
 }
 
+// ****************************************************************************
+// Stream a query result as CSV to an writeable stream (say, ExpressJS result
+// object or file stream)
+// ----
+// @param   Object      query       An RethinkDB Dash query object
+// @param   Object      out         The writeable stream
+// @param   Function`   cb          What to do on error and/or when done streaming
+// ****************************************************************************
 Rethink2Csv.prototype.generate_csv = function (query, out, cb) {
-    if (!query || typeof query !== 'object' || lo_functions(query).indexOf('toStream'))
-        return cb(Error("Invalid rethinkdb query object supplied"), null);
+    if (typeof query !== 'function' || lo_functions(query).indexOf('toStream') === -1)
+        return cb(Error('Invalid rethinkdb query object supplied'), null);
 
     var csv_writer = csv_write_stream(this.csv_opts);
 
@@ -36,7 +47,7 @@ Rethink2Csv.prototype.generate_csv = function (query, out, cb) {
         .pipe(csv_writer)
         .pipe(out)
         .on('finish', function() {
-            return cb(null, {msg: "Done writing query result to output stream!"});
+            return cb(null, {msg: 'Done writing query result to output stream!'});
         });
 };
 
